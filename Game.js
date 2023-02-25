1d761f4d2300c8fb4c81e69a5881ce5dca65429a
judeallanhill <judeallanhill@icloud.com>
2023-02-25 08:43:25 -0800

    Adding Menu SpaceShip And Planet

--- Game.js
+++ Game.js
@@ -1,6 +1,51 @@
 let canvas = document.getElementById('canvas');
 const ctx = canvas.getContext("2d");
 let currentKey = new Map();
+let mode = "splash"
+let navKeys = new Map();
+class Splash {
+    constructor() {
+        this.arrowLeft = new Image();
+        this.arrowRight = new Image();
+        this.arrowLeft.src = "./Assets/arrowLeft.png";
+        this.arrowRight.src = "./Assets/arrowRight.png";
+        this.x = 15
+        this.y = canvas.height/2;
+        this.up = false
+        this.down = false
+        this.scale = 1;
+    }
+    draw() {
+        ctx.fillStyle = "white"
+        ctx.fillRect(0, 0, canvas.width, canvas.height)
+        ctx.fillStyle = "black"
+        ctx.font = 30*this.scale + "px VT323";
+        ctx.fillText("Forbidden Plannet!", canvas.width / 2-95*this.scale, canvas.height / 2 - 40 * this.scale);
+        ctx.fillText("Press Space To Start!", canvas.width / 2-108*this.scale, canvas.height / 2 - 10 * this.scale)
+        ctx.imageSmoothingEnabled = false;
+        ctx.drawImage(this.arrowLeft,canvas.width/2+125 * this.scale + this.x,this.y * this.scale + 75,512/15 * this.scale ,512/15*this.scale)
+        ctx.drawImage(this.arrowRight,canvas.width/2-160 * this.scale + this.x,this.y * this.scale + 75,512/15 * this.scale ,512/15*this.scale)
+    }
+    animate() {
+        if (this.up === false) {
+            setTimeout(() => {
+                this.y += 1;
+                this.up = true
+                this.down = false
+                return
+            }, 120)
+        }
+        if (this.down === false) {
+            setTimeout(() => {
+                this.y -= 1;
+                this.down = true;
+                this.up = false;
+                return
+            }, 120)
+        }
+    }
+
+}
 class Point {
 	constructor(x,y) {
 		this.x = x
@@ -44,36 +89,6 @@ class Rect {
     return true;
 	}
 }
-function test_rects() {
-	let rect = new Rect(30,25,20,20)
-	console.assert(rect.contains(new Point(0,0))==false)
-	//Top
-	console.assert(rect.contains(new Point(30,5))==false)
-	//Inside
-	console.assert(rect.contains(new Point(30,30))==true)
-	//Right
-	console.assert(rect.contains(new Point(50,10))==false)
-	//Left
-	console.assert(rect.contains(new Point(5,10))==false)
-	//Botom
-	console.assert(rect.contains(new Point(25,60))==false)
-	let rect2 = new Rect(40,30,20,20)
-	console.assert(rect.intersects(rect2)==true)
-	console.assert(rect2.intersects(rect)==true)
-    if (rect.intersects(rect2) || rect2.intersects(rect)) {
-        log("WORKS")
-    }
-	console.log("TESTS PASS")
-}
-test_rects();
-function log(s) {
-    console.log(s)
-
-}
-function fillRect(x,y,w,h,color) {
-    ctx.fillStyle = color;
-    ctx.fillRect(x,y,w,h);
-}
 class Player {
     constructor() {
         this.image = new Image();
@@ -113,7 +128,7 @@ class Enemy {
         this.mad = false;
         this.image = new Image();
         this.image.src = "./Assets/GostNice.png"
-        this.bounds = new Rect(200,10,32*this.scale,32*this.scale);
+        this.bounds = new Rect(300,100,32*this.scale,32*this.scale);
     }
     draw() {
         if (this.alive === true) {
@@ -172,11 +187,11 @@ class ToolBar {
         this.x = 10
         this.y = 500
         this.size = 30;
-        this.barLength = 12
+        this.barLength = 8
     }
 
     draw() {
-        this.x = canvas.width /2 - 275
+        this.x = canvas.width /2 - 23*this.barLength
         this.y = canvas.height - 40
         ctx.lineWidth = 2
         ctx.strokeStyle = "white"
@@ -185,43 +200,124 @@ class ToolBar {
         }
     }
 }
+class BackPack {
+    constructor () {
+        this.visable = false;
+        this.w = 800
+        this.h = 550;
+    }
+    draw() {
+        if (this.visable === true) {
+            log("RUNNING")
+            ctx.fillStyle = "white"
+            ctx.globalAlpha = 0.9;
+            ctx.fillRect(canvas.width/2 - this.w/2,canvas.height/2 - this.h/2,this.w,this.h)
+            ctx.globalAlpha = 1.0;
+        }
+    }
+}
+class Items {
+    constructor() {
+        this.moon = new Image();
+        this.moon.src = "./Assets/Planet.png"
+        this.spaceShip = new Image();
+        this.spaceShip.src = "Assets/spaceShip.png";
+    }
+}
+let item = new Items();
+function test_rects() {
+	let rect = new Rect(30,25,20,20)
+	console.assert(rect.contains(new Point(0,0))==false)
+	//Top
+	console.assert(rect.contains(new Point(30,5))==false)
+	//Inside
+	console.assert(rect.contains(new Point(30,30))==true)
+	//Right
+	console.assert(rect.contains(new Point(50,10))==false)
+	//Left
+	console.assert(rect.contains(new Point(5,10))==false)
+	//Botom
+	console.assert(rect.contains(new Point(25,60))==false)
+	let rect2 = new Rect(40,30,20,20)
+	console.assert(rect.intersects(rect2)==true)
+	console.assert(rect2.intersects(rect)==true)
+    if (rect.intersects(rect2) || rect2.intersects(rect)) {
+        log("WORKS")
+    }
+	console.log("TESTS PASS")
+}
+test_rects();
+function log(s) {
+    console.log(s)
+
+}
+function fillRect(x,y,w,h,color) {
+    ctx.fillStyle = color;
+    ctx.fillRect(x,y,w,h);
+}
+let keyboardEnabled = true;
 let toolbar = new ToolBar()
 let heart = new Heart();
 let enemy = new Enemy();
+let player = new Player();
+let splash = new Splash();
+let backpack = new BackPack();
+
+
 function keyboardLoop() {
-    if (currentKey.get("w") === true) {
-        player.direction = "forward"
-        player.animate();
-        player.bounds.y -= player.speed
-      } 
-      if (currentKey.get("s") === true) {
-        player.direction = "back"
-        player.animate();
-        player.bounds.y += player.speed
-      } 
-      if (currentKey.get("a") === true) {
-        player.direction = "left"
-        player.animate();
-        player.bounds.x -= player.speed
-      } 
-      if (currentKey.get("d") === true) {
-        player.direction = "right"
-        player.animate();
-        player.bounds.x += player.speed
-      } 
+    if (keyboardEnabled) {}
+
+    if (mode === "splash") {
+        if (currentKey.get(" ") === true) {
+            mode = "game"
+        }   
+    }
+
+    if (mode === "game") {
+        if (keyboardEnabled) {
+            if (currentKey.get("w") === true) {
+                player.direction = "forward"
+                player.animate();
+                player.bounds.y -= player.speed
+              } 
+              if (currentKey.get("s") === true) {
+                player.direction = "back"
+                player.animate();
+                player.bounds.y += player.speed
+              } 
+              if (currentKey.get("a") === true) {
+                player.direction = "left"
+                player.animate();
+                player.bounds.x -= player.speed
+              } 
+              if (currentKey.get("d") === true) {
+                player.direction = "right"
+                player.animate();
+                player.bounds.x += player.speed
+              } 
+        }
+        if (navKeys.get("e") === true) {
+            backpack.visable = !backpack.visable
+            keyboardEnabled = false
+        }
+        if (backpack.visable === false) {
+            keyboardEnabled = true
+
+        }
+    }
 }
 function keyboardInit() {
     window.addEventListener("keydown", function (event) {
         currentKey.set(event.key, true);
-    
+        navKeys.set(event.key, true);
+
       });
       window.addEventListener("keyup", function (event) {
         currentKey.set(event.key, false);
-        currentKey.set(event.key, false);
+        navKeys.set(event.key, false);
     
       });    
 }
-let player = new Player();
 function ResizeCanvas() {
     canvas.width = window.innerWidth -30
     canvas.height = window.innerHeight -30
@@ -231,15 +327,23 @@ function clearScreen() {
 }
 function loop() {
     clearScreen();
-    player.colison();
-    heart.colison();
-    enemy.follow();
     keyboardLoop();
-    enemy.animate();
-    enemy.draw();
-    player.draw();
-    heart.draw();
-    toolbar.draw();
+    if (mode === "game") {
+        player.colison();
+        heart.colison();
+        enemy.follow();
+        enemy.animate();
+        enemy.draw();
+        player.draw();
+        heart.draw();
+        toolbar.draw();
+        backpack.draw();
+    }
+    if (mode === "splash") {
+        splash.draw();
+        splash.animate();
+    }
+    navKeys.clear();
     requestAnimationFrame(loop);
 }
 function init() {
